316dd80495e90ce95e2ea88958818505378f98c8
Shaun Evans <shaun.evans@high-view-studios.co.uk>
2020-06-16 11:44:57 +0100

    test the .env in the file

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
+require('dotenv').config();
 const express = require('express');
 
 const app = express();
 
+
 app.get('/', (req, res) => {
-    res.send("open at High-View Studios")
+    //res.sendFile(__dirname + "/index.html");
+    res.write("<h1>hello</h1></br>");
+    res.write(process.env.DB_HOST);
+    res.send();
 });
 
 app.listen(process.env.PORT || 8080, (req, res) => {
